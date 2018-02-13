36848ef6128d967ab5dc7bb99aa614a11b6ba4fb
KevAli <kev@cqu.edu.cn>
2018-02-13 21:17:10 +0800

    version 1.1.1

--- pythonPractice/pythonbasics/deeplearningandcomputervison/p175_01_matplotlib.py
+++ pythonPractice/pythonbasics/deeplearningandcomputervison/p175_01_matplotlib.py
@@ -9,7 +9,14 @@ np.random.seed(42)
 x = np.linspace(0, 5, 100)
 y = 2 * np.sin(x) + 0.3 * x ** 2
 y_data = y + np.random.normal(scale=0.3, size=100)
+
 plt.figure('data')
 plt.plot(x, y_data, '.')
+plt.figure('model')
+plt.plot(x, y)
+plt.figure('data & model')
+plt.plot(x, y, 'k', lw=3)
+plt.scatter(x, y_data)
+plt.savefig('/home/kevin/Desktop/GitSource/result.png')
 
 plt.show()
